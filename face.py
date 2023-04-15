8eeabd6b0efb5fd2809ac34ef4d97bf1e10b470b
Aditya <adityanjr2064@gmail.com>
2023-04-16 02:12:21 +0530

    first commit

---
+++ face.py
@@ -0,0 +1,42 @@
+import cv2
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+import datetime
+from keras.utils import load_img
+from keras.utils import img_to_array
+
+mymodel = load_model('FaceMask.h5')
+
+cap = cv2.VideoCapture(0)
+
+##It uses the function detectMultiScale to detect the upper left corner of the rectangle (x, y) on the face as well as width and height of the rectangle. detectMultiScale function has parameters such as scaling and neighbours.
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while cap.isOpened():
+    _, img = cap.read()
+    face = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=4)
+    for (x, y, w, h) in face:
+        face_img = img[y:y + h, x:x + w]
+        cv2.imwrite('temp.jpg', face_img)
+        test_image = load_img('temp.jpg', target_size=(150, 150, 3))
+        test_image = img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        pred = mymodel.predict(test_image)[0][0]
+        if pred == 1:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 3)
+            cv2.putText(img, 'NO MASK', ((x + w) // 2, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+        else:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            cv2.putText(img, 'MASK', ((x + w) // 2, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+        datet = str(datetime.datetime.now())
+        cv2.putText(img, datet, (400, 450), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+    cv2.imshow('img', img)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
